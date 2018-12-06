eeb0b626a2f62752930f343f01a7aff9611ae04e
Shayna bauer <shayna.bauer@acolconsultoria.com>
2018-12-06 17:40:30 -0200

    criando providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { HttpModule } from "@angular/http";
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -9,6 +10,7 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { ProductPageModule } from '../pages/product/product.module';
+import { ProdutosProvider } from '../providers/produtos/produtos';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,8 @@ import { ProductPageModule } from '../pages/product/product.module';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    ProductPageModule
+    ProductPageModule,
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -30,7 +33,8 @@ import { ProductPageModule } from '../pages/product/product.module';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ProdutosProvider
   ]
 })
 export class AppModule {}
---
+++ src/providers/produtos/produtos.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the ProdutosProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class ProdutosProvider {
+
+  constructor(public http: HttpClient) {
+    console.log('Hello ProdutosProvider Provider');
+  }
+
+  getProdutos() {
+    return this.http.get("https://...");
+  }
+
+}
